36c7f65d3f466bca71f5f4cb6e7f8e69d4b38701
ImperatorNeron <katebarspk@gmail.com>
2024-04-29 13:26:25 +0300

    Changes

--- hanbild/settings.py
+++ hanbild/settings.py
@@ -10,10 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# DEBUG = os.getenv("DEBUG") == "True"
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '1') == '1'
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 INSTALLED_APPS = [
     "modeltranslation",
