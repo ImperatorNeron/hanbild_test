8aa59f303c36a544cba21b7a1fa3a92f11020077
ImperatorNeron <katebarspk@gmail.com>
2024-06-15 19:20:18 +0300

    Added robots.txt

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 from main import views
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path("contacts/", views.ContactsView.as_view(), name="contacts"),
     path("services/", views.ServicesView.as_view(), name="services"),
     path("privacy-policy/", views.PrivacyPolicyView.as_view(), name="privacy_policy"),
+    re_path(r'^robots\.txt$', TemplateView.as_view(template_name="config/robots.txt", content_type="text/plain")),
 ]
