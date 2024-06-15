0d4fc9ff70dfc74ef8f5b1cbe9f10e040c3d5ac5
ImperatorNeron <katebarspk@gmail.com>
2024-06-15 20:54:10 +0300

    Added sitemap.xml

--- main/urls.py
+++ main/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("services/", views.ServicesView.as_view(), name="services"),
     path("privacy-policy/", views.PrivacyPolicyView.as_view(), name="privacy_policy"),
     re_path(r'^robots\.txt$', TemplateView.as_view(template_name="config/robots.txt", content_type="text/plain")),
+    re_path(r'^sitemap\.xml$', TemplateView.as_view(template_name="config/sitemap.xml", content_type="application/xml")),
 ]
