e158073a0c82de37fa8d37453e061880f706d2e6
ImperatorNeron <katebarspk@gmail.com>
2024-08-02 15:23:15 +0300

    #25 | SEO fixing | Added new url for delivery

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("contacts/", views.ContactsView.as_view(), name="contacts"),
     path("services/", views.ServicesView.as_view(), name="services"),
+    path("delivery/", views.DeliveryView.as_view(), name="delivery"),
     path("privacy-policy/", views.PrivacyPolicyView.as_view(), name="privacy_policy"),
     re_path(r'^robots\.txt$', TemplateView.as_view(template_name="config/robots.txt", content_type="text/plain")),
     re_path(r'^sitemap\.xml$', TemplateView.as_view(template_name="config/sitemap.xml", content_type="application/xml")),
